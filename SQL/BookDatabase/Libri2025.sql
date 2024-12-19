c6b26403fa1a5a867b89579992baf568fa6c604d
Lisa <dimarcolisa@outlook.com>
2024-12-19 11:06:32 +0100

    Create Libri2025.sql

---
+++ SQL/BookDatabase/Libri2025.sql
@@ -0,0 +1,13 @@
+--Libri che prevedo di leggere nel 2025--
+CREATE TABLE books_2025_prevision (
+  id integer primary key,
+  name text,
+  author text,
+  date date
+  );
+
+INSERT TABLE (id, name, author, date)
+VALUES
+(1, 'Interpretazione dei sogni', 'Sigmund Freud', 2025-2-29),
+(2, 'Sapiens', 'Yuval Noah Harari', 2025-4-30),
+(3, 
