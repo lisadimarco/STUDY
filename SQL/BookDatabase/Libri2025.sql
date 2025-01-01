5fc36040a0a48150f63c4555e6761259abda96f5
Lisa <dimarcolisa@outlook.com>
2025-01-01 09:32:57 +0100

    Update Libri2025.sql

--- SQL/BookDatabase/Libri2025.sql
+++ SQL/BookDatabase/Libri2025.sql
@@ -8,6 +8,9 @@ CREATE TABLE books_2025_prevision (
 
 INSERT TABLE (id, name, author, date)
 VALUES
-(1, 'Interpretazione dei sogni', 'Sigmund Freud', 2025-2-29),
-(2, 'Sapiens', 'Yuval Noah Harari', 2025-4-30),
-(3, 
+  (1, 'Perche dormiamo', 'Matthew Walker', '2025-1-30')
+  (2, 'Una vita degna di essere vissuta', 'Marsha Linehan' '2025-2-28')
+  (3, 'Donne che corrono coi lupi', 'Clarissa Pinkola Estes' '2025-4-15')
+  (4, 'Interpretazione dei sogni', 'Sigmund Freud', '2025-5-29'),
+  (5, 'Sapiens', 'Yuval Noah Harari', '2025-7-30'),
+  (6, 'La brevita della vita', 'Seneca', '2025-8-10')
