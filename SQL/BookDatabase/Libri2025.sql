643956801b895da7d670907f4f45e501157fcdd8
Lisa <dimarcolisa@outlook.com>
2025-01-10 11:14:21 +0100

    Update Libri2025.sql
    editing some syntax errors

--- SQL/BookDatabase/Libri2025.sql
+++ SQL/BookDatabase/Libri2025.sql
@@ -1,16 +1,17 @@
 --Libri che prevedo di leggere nel 2025--
 CREATE TABLE books_2025_prevision (
-  id integer primary key,
-  name text,
-  author text,
-  date date
-  );
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  author TEXT,
+  date DATE
+);
 
-INSERT TABLE (id, name, author, date)
+INSERT INTO books_2025_prevision (id, name, author, date)
 VALUES
-  (1, 'Perche dormiamo', 'Matthew Walker', '2025-1-30')
-  (2, 'Una vita degna di essere vissuta', 'Marsha Linehan' '2025-2-28')
-  (3, 'Donne che corrono coi lupi', 'Clarissa Pinkola Estes' '2025-4-15')
-  (4, 'Interpretazione dei sogni', 'Sigmund Freud', '2025-5-29'),
-  (5, 'Sapiens', 'Yuval Noah Harari', '2025-7-30'),
-  (6, 'La brevita della vita', 'Seneca', '2025-8-10')
+  (1, 'Perche dormiamo', 'Matthew Walker', '2025-01-30'),
+  (2, 'Una vita degna di essere vissuta', 'Marsha Linehan', '2025-02-28'),
+  (3, 'Donne che corrono coi lupi', 'Clarissa Pinkola Estes', '2025-04-15'),
+  (4, 'Interpretazione dei sogni', 'Sigmund Freud', '2025-05-29'),
+  (5, 'Sapiens', 'Yuval Noah Harari', '2025-07-30'),
+  (6, 'La brevita della vita', 'Seneca', '2025-08-10');
+
