c801e3ca822b93e22502533af2df4e67b393ec08
Lisa <dimarcolisa@outlook.com>
2025-07-05 16:23:30 +0200

    Update Libri2025.sql

--- SQL/BookDatabase/Libri2025.sql
+++ SQL/BookDatabase/Libri2025.sql
@@ -9,9 +9,10 @@ CREATE TABLE books_2025_prevision (
 INSERT INTO books_2025_prevision (id, name, author, date)
 VALUES
   (1, 'Perche dormiamo', 'Matthew Walker', '2025-03-30'),
-  (2, 'Una vita degna di essere vissuta', 'Marsha Linehan', '2025-06-28'),
-  (3, 'Donne che corrono coi lupi', 'Clarissa Pinkola Estes', '2025-08-15'),
-  (4, 'Donne che pensano troppo', 'Susan Nolen‑Hoeksema', '2025-09-29'),
-  (5, 'Sapiens', 'Yuval Noah Harari', '2025-07-30'),
-  (6, 'La brevita della vita', 'Seneca', '2025-08-10');
+  (2, 'Una vita degna di essere vissuta', 'Marsha Linehan', '2025-05-28'),
+  (3, 'La campana di vetro', 'Sylvia Plath', '2025-06-26'),
+  (4, 'Donne che corrono coi lupi', 'Clarissa Pinkola Estes', '2025-08-15'),
+  (5, 'Donne che pensano troppo', 'Susan Nolen‑Hoeksema', '2025-09-29'),
+  (6, 'Sapiens', 'Yuval Noah Harari', '2025-07-30'),
+  (7, 'La brevita della vita', 'Seneca', '2025-08-10');
 
