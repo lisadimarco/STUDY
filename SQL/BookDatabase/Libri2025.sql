58c9e43e738f0ed3bb0112277ea7412c10465ae9
Lisa <dimarcolisa@outlook.com>
2025-07-06 16:54:38 +0200

    Update Libri2025.sql

--- SQL/BookDatabase/Libri2025.sql
+++ SQL/BookDatabase/Libri2025.sql
@@ -11,8 +11,11 @@ VALUES
   (1, 'Perche dormiamo', 'Matthew Walker', '2025-03-30'),
   (2, 'Una vita degna di essere vissuta', 'Marsha Linehan', '2025-05-28'),
   (3, 'La campana di vetro', 'Sylvia Plath', '2025-06-26'),
-  (4, 'Donne che corrono coi lupi', 'Clarissa Pinkola Estes', '2025-08-15'),
-  (5, 'Donne che pensano troppo', 'Susan Nolen‑Hoeksema', '2025-09-29'),
-  (6, 'Sapiens', 'Yuval Noah Harari', '2025-07-30'),
-  (7, 'La brevita della vita', 'Seneca', '2025-08-10');
+  (4, 'Donne che pensano troppo', 'Susan Nolen‑Hoeksema', '2025-07-15'),
+  (5, 'Donne che corrono coi lupi', 'Clarissa Pinkola Estes', '2025-08-20'),
+  (6, 'Il corpo accusa il colpo', 'Bessel van der Kolk', '2025-09-30'),
+  (7, 'La brevita della vita', 'Seneca', '2025-10-10');
+  (8, 'Follia', 'Patrick McGrath', '2025-10-30');
+  (9, 'La dea dentro la donna','Jean S. Bolen','2025-11-30');
+  (10, '','','');
 
